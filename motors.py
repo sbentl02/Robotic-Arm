eeae10b303185d06364a1620507167be4fda9a3d
Stephanie Bentley <sc.bentley@yahoo.com>
2021-10-10 15:58:19 -0400

    Add motor controller

---
+++ motors.py
@@ -0,0 +1,12 @@
+import time
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=16)
+
+kit.servo[0].angle = 180
+kit.continuous_servo[1].throttle = 1
+time.sleep(1)
+kit.continuous_servo[1].throttle = -1
+time.sleep(1)
+kit.servo[0].angle = 0
+kit.continuous_servo[1].throttle = 0
