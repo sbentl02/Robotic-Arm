680ddb694562b05f20c438620600715b6f0e9e94
jackamsbry <60232817+jackamsbry@users.noreply.github.com>
2021-10-12 00:10:03 -0400

    angles fixy

--- motors.py
+++ motors.py
@@ -29,20 +29,24 @@ def IK_Solve(x, y, z):
     print(x_w, y_w)
     
     theta3 = math.pi - math.acos((length1**2 + length2**2 - x_w**2 - y_w**2)/(2*length1*length2))    
-    theta3 = math.pi - theta3
+    theta3_out = math.pi - theta3
 
     alpha = math.atan2(y_w, x_w)
 
     theta2 = alpha - math.acos((x_w**2 + y_w**2 + length1**2 - length2**2)/(2*length1*math.sqrt(x_w**2 + y_w**2)))
-    theta2 = math.pi - theta2
+    theta2_out = math.pi - theta2
     theta4 = phi_e - theta2 - theta3
-    
+    theta4_out = math.pi - theta4
 
     print(theta2)
     print(theta3)
     print(theta4)
-
-    return [theta1, theta2, theta3, theta4]
+    rad2deg = 180/math.pi
+    theta1_out = theta1 * rad2deg
+    theta2_out = theta2_out * rad2deg
+    theta3_out = theta3_out * rad2deg
+    theta4_out = theta4_out * rad2deg
+    return [theta1_out, theta2_out, theta3_out, theta4_out]
 # kit.servo[0].angle = 180
 # kit.continuous_servo[1].throttle = 1
 # time.sleep(1)
