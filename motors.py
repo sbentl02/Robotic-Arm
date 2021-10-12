0dfa75eb05d7b8ace46cc0c2b03d13997c4bd66f
jackamsbry <60232817+jackamsbry@users.noreply.github.com>
2021-10-12 00:22:32 -0400

    number

--- motors.py
+++ motors.py
@@ -36,7 +36,7 @@ def IK_Solve(x, y, z):
     theta2 = alpha - math.acos((x_w**2 + y_w**2 + length1**2 - length2**2)/(2*length1*math.sqrt(x_w**2 + y_w**2)))
     theta2_out = math.pi + theta2
     theta4 = phi_e - theta2 - theta3
-    theta4_out = math.pi + theta4
+    theta4_out = math.pi/2 + theta4
 
     rad2deg = 180/math.pi
     theta1_out = theta1 * rad2deg
